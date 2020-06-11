bd0789881394afffc3dd426c9c6d5c172c9fd15a
GCdM <59828466+GCdM@users.noreply.github.com>
2020-06-11 22:08:11 +0100

    Set up problem two, with tests

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,9 @@ fn main() {
         Multiples of 3 and 5 */ 
     let input_one = 1000;
     println!("Solution 1: ({}) -> {}", input_one, problem::one::solve(input_one));
+
+    /* Problem 2:
+        Even Fibonacci numbers */
+    let input_two = 4000000;
+    println!("Solution 2: ({}) -> {}", input_two, problem::two::solve(input_two));
 }
--- src/problem/one.rs
+++ src/problem/one.rs
@@ -1,9 +1,9 @@
 /* Problem 1: 
     Multiples of 3 and 5
 
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
-Find the sum of all the multiples of 3 or 5 below 1000.
+    Find the sum of all the multiples of 3 or 5 below 1000. 
 */
 
 pub fn solve(upper_limit: i32) -> i32 {
@@ -27,7 +27,7 @@ fn is_divisible_by_5(value: &i32) -> bool {
 }
 
 #[cfg(test)]
-mod tests {
+mod p1_tests {
     use super::*;
 
     #[test]
